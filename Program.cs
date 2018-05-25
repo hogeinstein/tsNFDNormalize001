5887703022116efb1ecb7820975ccc5582ecf832
hogeinstein <hogeinstein@gmail.com>
2018-05-25 16:21:38 +0900

    Shift_JIS文字列変化にも対応

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace tsNFDNormalize001
 {
@@ -10,13 +11,18 @@ namespace tsNFDNormalize001
     {
         static void Main(string[] args)
         {
+            var log = new StringBuilder();
             foreach (var item in args)
             {
-                ConvNFDNormalize(item);
+                ConvNFDNormalize(item, ref log);
+            }
+            if (log.Length != 0)
+            {
+                var res = MessageBox.Show(log.ToString(), @"失敗", MessageBoxButtons.OK);
             }
         }
 
-        private static void ConvNFDNormalize(string path)
+        private static void ConvNFDNormalize(string path, ref StringBuilder log)
         {
             try
             {
@@ -33,21 +39,38 @@ namespace tsNFDNormalize001
                 {
                     var dir_name = System.IO.Path.GetDirectoryName(path) + System.IO.Path.DirectorySeparatorChar;
                     var name1 = System.IO.Path.GetFileName(path);
-                    // 全角の濁点・半濁点を、UNICODE二文字目の濁点・半濁点に置換後に、Normalize
-                    var name2 = name1
-                        .Replace("\u309b", "\u3099")
-                        .Replace("\u309c", "\u309a")
-                        .Normalize();
-                    if (name1 != name2)
+
+                    var name2 = ConvSjisCode(name1);
+                    var name3 = ConvDakuten(name2);
+                    if (name1 != name3)
                     {
-                        func(dir_name + name1, dir_name + name2);
+                        func(dir_name + name1, dir_name + name3);
                     }
                 }
             }
             catch (Exception e)
             {
-                System.Console.WriteLine(@"conv err {0} : {1}", path, e.Message);
+                log.AppendFormat(@"conv err {0} : {1}\r\n", path, e.Message);
             }
+        }
+
+        private static string ConvDakuten(string in_str)
+        {
+            // 全角の濁点・半濁点を、UNICODE二文字目の濁点・半濁点に置換後に、Normalize
+            return in_str
+                .Replace("\u309b", "\u3099")
+                .Replace("\u309c", "\u309a")
+                .Normalize();
+        }
+
+        private static string ConvSjisCode(string in_str)
+        {
+            // Shift_JISで表現できない文字列を削除する
+            Encoding sjisEnc = Encoding.GetEncoding(@"Shift_JIS");
+            byte[] sjis_byte = sjisEnc.GetBytes(in_str);
+            var str1 = sjisEnc.GetString(sjis_byte);
+            var str2 = str1.Replace(@"?", @"");
+            return str2;
         }
     }
 }
